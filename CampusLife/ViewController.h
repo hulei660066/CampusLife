4e074ec12551f963110a51d3b9a291223921a25a
lane <lane@lanedeMacBook-Air.local>
2015-07-01 18:30:47 +0800

    Initial Commit

---
+++ CampusLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CampusLife
+//
+//  Created by lane on 15/7/1.
+//  Copyright (c) 2015年 LANE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CampusLife/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CampusLife
+//
+//  Created by lane on 15/7/1.
+//  Copyright (c) 2015年 LANE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
